1d24e60429d812b9a2beaf6dd9b3df365e627aa2
James Cameron Abreu <coolcam222@gmail.com>
2019-10-15 18:04:14 -0400

    Initial project creation
    Blank

---
+++ CobaltGameJam/Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Test : MonoBehaviour
+    {
+        void Update()
+        {
+            Debug.Log("Testing...");
+
+        }
+    }
+}
