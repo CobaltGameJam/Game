f08eded3afad2cba06ef4fde073f68e5b326f7b6
James Cameron Abreu <coolcam222@gmail.com>
2019-11-04 16:59:06 -0500

    Machine Gun fire
    + Hold down mouse to fire like machine gun
    + Bullets get destroyed outside camera

---
+++ Test123/Assets/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 screenPos = mainCamera.WorldToScreenPoint(transform.position);
+        if (screenPos.x < 0 || screenPos.x > Screen.width || screenPos.y < 0 || screenPos.y > Screen.height)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Test123/Assets/Scripts/PlayableCharacter.cs
+++ Test123/Assets/Scripts/PlayableCharacter.cs
@@ -5,37 +5,54 @@ using UnityEngine;
 public class PlayableCharacter : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speed = .5f;
+    public float moveSpeed = 0.4f;
+    public float bulletSpeed = 15f;
     public Rigidbody2D ridB;
     public Rigidbody2D bullet;
+
+    public int StepsBetweenFire = 20;
+
+    private int fireAlarm;
+    
 
     void Start()
     {
         ridB = GetComponent<Rigidbody2D>();
+        fireAlarm = 0;
     }
 
     void FixedUpdate()
     {
+        // Movement
         Vector2 movingV = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        ridB.transform.Translate(movingV * speed);
+        movingV *= Time.deltaTime;
+        ridB.transform.Translate(movingV * moveSpeed);
 
-
-        if(Input.GetButtonDown("Fire1"))
+        // Fire Weapon
+        fireAlarm--;
+        if(Input.GetButton("Fire1"))
         {
-            Rigidbody2D clone;
-            clone = Instantiate(bullet, transform.position + transform.right * 1, transform.rotation);
+            
+            if (fireAlarm <= 0)
+            {
+                Rigidbody2D clone;
+                clone = Instantiate(bullet, transform.position + transform.right * 1, transform.rotation);
 
+                var force = transform.right * bulletSpeed;
+                Debug.Log($"force: {force}");
+                clone.AddForce(force, ForceMode2D.Impulse);
 
-
-            var force = transform.right * speed;
-            Debug.Log($"force: {force}");
-            clone.AddForce(force, ForceMode2D.Impulse);
+                // reset alarm:
+                fireAlarm = StepsBetweenFire;
+            }
         }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FixedUpdate();
     }
 }
